91e95789769ed03cd8f550b853c8a3e866b74b47
Sushkov AS <aveslaizer@gmail.com>
2023-03-17 23:02:28 +0300

    Написал класс Human

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+
 """
 Задание 1.
 Реализуйте класс «Человек». Необходимо хранить в полях класса: ФИО,
@@ -5,12 +8,27 @@
 Реализуйте конструктор по умолчанию и метод для вывода данных.
 Реализуйте доступ к отдельным полям класса через методы класса (геттеры
 и сеттеры).
-Задание 2.
-Реализуйте класс «Книга». Необходимо хранить в полях класса:
-название книги, год выпуска, издателя, жанр, автора, цену. Реализуйте
-конструктор по умолчанию и метод для вывода данных. Реализуйте доступ к
-отдельным полям класса через методы класса (геттеры и сеттеры).
 """
+
+
+class Human:
+
+    def __init__(self, name: Dict[str, str], birthday: Dict[str, str], phone: str,
+                 country: str, city: str, address: Dict[str, str]):
+        self.name = name
+        self.birthday = birthday
+        self.phone = phone
+        self.country = country
+        self.city = city
+        self.address = address
+
+    def __str__(self):
+        return f"ФИО: {list(self.name.values())}\n" \
+               f"Дата рождения: {list(self.birthday.values())}\n" \
+               f"Номер телефона: {self.phone}\n" \
+               f"Страна: {self.country}\n" \
+               f"Город: {self.city}\n" \
+               f"Адрес: {list(self.address.values())}\n"
 
 def execute_application():
     pass
