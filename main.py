6560cad167182197323ea4bda31fe98ee610fe49
Sushkov AS <aveslaizer@gmail.com>
2023-03-17 22:12:39 +0300

    Написал конструктор класса Stadium

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ class Stadium:
     city: str
     capacity: int
 
+    def __init__(self, name: str, date: Dict[str, str],
+                 country: str, city: str, capacity: int):
+        self.name = name
+        self.date_of_opening = date
+        self.country = country
+        self.city = city
+        self.capacity = capacity
+
+
 
 def execute_application():
     pass
