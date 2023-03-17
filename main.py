2e6de00470acd624aeb9e04526b07a49a111ec79
Sushkov AS <aveslaizer@gmail.com>
2023-03-18 00:16:04 +0300

    Протестировал геттеры и сеттеры

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from typing import Dict
 
 
 class Human:
+    name: Dict[str, str]
 
     def __init__(self, name: Dict[str, str], birthday: Dict[str, str], phone: str,
                  country: str, city: str, address: Dict[str, str]):
@@ -30,10 +31,82 @@ class Human:
                f"Город: {self.__city}\n" \
                f"Адрес: {list(self.__address.values())}\n"
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name: Dict[str, str]):
+        self.__name = name.copy()
+
+    @property
+    def birthday(self):
+        return self.__birthday
+
+    @birthday.setter
+    def birthday(self, birthday: Dict[str, str]):
+        self.__birthday = birthday.copy()
+
+
+    def get_phone(self):
+        return self.__phone
+
+
+    def set_phone(self, phone):
+        self.__phone = phone
+
+    @property
+    def country(self):
+        return self.__country
+
+    @country.setter
+    def country(self, country: str):
+        self.__country = country
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, city:str):
+        self.__city = city
+
+    @property
+    def address(self):
+        return self.__address
+
+    @address.setter
+    def address(self, address: Dict[str, str]):
+        self.__address = address.copy()
+
 
 
 def execute_application():
-    pass
+    birthday = {
+        "day": "12",
+        "month": "01",
+        "year": "1234"
+    }
+    name = {
+        "фамилия": "Сушков",
+        "имя": "Артем",
+        "отчество": "Сергеевич"
+    }
+    address = {
+        "улица": "Пушкина",
+        "дом": "1"
+    }
+    man = Human(name, birthday, "464646", "Русь", "Тутаево", address)
+    print(man)
+    birthday["day"] = "99999"
+    print(man)
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
