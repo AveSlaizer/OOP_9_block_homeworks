0e7b97d172bc616a61f887b0c54af99059ddce6f
Sushkov AS <aveslaizer@gmail.com>
2023-03-17 22:15:25 +0300

    Переопределил строковый метод класс Stadium

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ class Stadium:
         self.city = city
         self.capacity = capacity
 
+    def __str__(self):
+        return f"Название: {self.name}\n" \
+               f"Дата открытия: {list(self.date_of_opening.values())}\n" \
+               f"Страна: {self.country}\n" \
+               f"Город: {self.city}\n" \
+               f"Вместимость: {self.capacity}\n"
 
 
 def execute_application():
