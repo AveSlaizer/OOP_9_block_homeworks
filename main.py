e09a9605b276cda7e9d970c05925bd746021adb1
Sushkov AS <aveslaizer@gmail.com>
2023-03-19 11:30:24 +0300

    Написал класс Car

--- main.py
+++ main.py
@@ -5,12 +5,28 @@
 цену. Реализуйте конструктор по умолчанию и метод для вывода данных.
 Реализуйте доступ к отдельным полям класса через методы класса (геттеры
 и сеттеры).
-Задание 2.
-Реализуйте класс «Стадион». Необходимо хранить в полях класса:
-название стадиона, дату открытия, страну, город, вместимость. Реализуйте
-конструктор по умолчанию и метод для вывода данных. Реализуйте доступ к
-отдельным полям класса через методы класса (геттеры и сеттеры).
 """
+
+
+class Car:
+
+    def __init__(self, brand: str, model: str, year: int,
+                 engine_volume: float, color: str, price: float):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.engine_volume = engine_volume
+        self.color = color
+        self.price = price
+
+    def __str__(self):
+        return f"Марка: {self.brand}\n" \
+               f"Модель: {self.model}\n" \
+               f"Год выпуска: {self.year}\n" \
+               f"Объем двигателя: {self.engine_volume}\n" \
+               f"Цвет: {self.color}\n" \
+               f"Цена: {self.price}\n"
+
 
 def execute_application():
     pass
