2755a331d5a774bf37f05ce0f9c02ea6eb3a7c7d
Sushkov AS <aveslaizer@gmail.com>
2023-03-19 11:52:30 +0300

    Написал геттры и сеттеры в классе Stadium

--- main.py
+++ main.py
@@ -26,6 +26,36 @@ class Stadium:
                f"Город: {self.__city}\n" \
                f"Вместимость: {self.__capacity}\n"
 
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def get_date(self):
+        return self.__date_of_opening
+
+    def set_date(self, date: Dict[str, str]):
+        self.__date_of_opening = date
+
+    def get_country(self):
+        return self.__country
+
+    def set_country(self, country: str):
+        self.__country = country
+
+    def get_city(self):
+        return self.__city
+
+    def set_city(self, city: str):
+        self.__city = city
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def set_capacity(self, capacity: float):
+        self.__capacity = capacity
+
 
 def execute_application():
     pass
