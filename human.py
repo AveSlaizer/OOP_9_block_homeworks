ad0ec9c57de97737151103ac22a466995b9f7786
Sushkov AS <aveslaizer@gmail.com>
2023-03-25 20:05:15 +0300

    Добавил геттеры и сеттеры классу Human

--- human.py
+++ human.py
@@ -19,3 +19,51 @@ class Human:
                f"Страна: {self.country}\n" \
                f"Город: {self.city}\n" \
                f"Адрес: {list(self.address.values())}\n"
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name: Dict[str, str]):
+        self.__name = name.copy()
+
+    @property
+    def birthday(self):
+        return self.__birthday
+
+    @birthday.setter
+    def birthday(self, birthday: Dict[str, str]):
+        self.__birthday = birthday.copy()
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, phone):
+        self.__phone = phone
+
+    @property
+    def country(self):
+        return self.__country
+
+    @country.setter
+    def country(self, country: str):
+        self.__country = country
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, city: str):
+        self.__city = city
+
+    @property
+    def address(self):
+        return self.__address
+
+    @address.setter
+    def address(self, address: Dict[str, str]):
+        self.__address = address.copy()
