2ebebb047ac31c25795efb937c8209ee68100e17
Sushkov AS <aveslaizer@gmail.com>
2023-03-26 02:44:37 +0300

    Написал классовый метод для класса Human

--- human.py
+++ human.py
@@ -20,6 +20,32 @@ class Human:
                f"Город: {self.city}\n" \
                f"Адрес: {list(self.address.values())}\n"
 
+
+    #строка в файле имеет вид:
+    #Иванов_Иван_Иванович 01_01_1999 +79091112233 Россия Ярославль Пушкина_5_97\n
+    @classmethod
+    def init_from_str(cls, a_string: str):
+        """
+        Возвращает объект класса Human. Данные для полей получает из переданной строки
+
+        :param a_string (str): Строка с данными
+        :return:
+                Объект класса Human
+        """
+        temp = tuple(a_string.rstrip().split())
+        temp_name = temp[0].split("_")
+        name = {"Фамилия": temp_name[0], "Имя": temp_name[1], "Отчество": temp_name[2]}
+        temp_birthday = temp[1].split("_")
+        birthday = {"день": temp_birthday[0], "месяц": temp_birthday[1], "год": temp_birthday[2]}
+        phone = temp[2]
+        country = temp[3]
+        city = temp[4]
+        temp_address = temp[5].split("_")
+        address = {"страна": country, "город": city, "улица": temp_address[0],
+                   "номер дома": temp_address[1], "квартира": temp_address[2]}
+        return cls(name, birthday, phone, country, city, address)
+
+
     @property
     def name(self):
         return self.__name
