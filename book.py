8c400c53a93a86bba8e5edc9d1a7ecee29434c15
Sushkov AS <aveslaizer@gmail.com>
2023-03-26 12:57:21 +0300

    Добавил геттеры и сеттеры в класс Book

--- book.py
+++ book.py
@@ -16,3 +16,51 @@ class Book:
                f"Издатель: {self.__publisher}\n" \
                f"Жанр: {self.__genre}\n" \
                f"Цена: {self.__price}\n"
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        self.__name = name
+
+    @property
+    def author(self):
+        return self.__author
+
+    @author.setter
+    def author(self, author: str):
+        self.__author = author
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, year: str):
+        self.__year = year
+
+    @property
+    def publisher(self):
+        return self.__publisher
+
+    @publisher.setter
+    def publisher(self, publisher: str):
+        self.__publisher = publisher
+
+    @property
+    def genre(self):
+        return self.__genre
+
+    @genre.setter
+    def genre(self, genre: str):
+        self.__genre = genre
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price: float):
+        self.__price = price
