6272c262a2a1209adde515e98a56d1db1dc16f84
Sushkov AS <aveslaizer@gmail.com>
2023-03-26 18:40:06 +0300

    Протестировал статические методы класса в основной программе

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ from temperature_formatter import TemperatureFormatter
 
 
 def execute_application():
-    pass
+    celsius = float(input("Введите температуру в градусах Цельсия: "))
+    fahrenheit = TemperatureFormatter.celsius_to_fahrenheit(celsius)
+    print(f"{celsius} градусов Цельсия равно {fahrenheit:.01f} градусов Фаренгейта.")
+
+    fahrenheit = float(input("Введите температура в градусах Фаренгейта: "))
+    celsius = TemperatureFormatter.fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit} градусов Фаренгейта равно {celsius:.01f} градусов Цельсия")
 
 
 if __name__ == "__main__":
