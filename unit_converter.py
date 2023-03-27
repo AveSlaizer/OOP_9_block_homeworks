aab9adb2d248a874279e6e87ea336fa9823c4009
Sushkov AS <aveslaizer@gmail.com>
2023-03-27 21:03:02 +0300

    Написал статический метод kilometers_to_miles

--- unit_converter.py
+++ unit_converter.py
@@ -1,2 +1,12 @@
 class UnitConverter:
-    pass
+
+    @staticmethod
+    def kilometers_to_miles(kilometers: float) -> float:
+        """
+        Преобразует километры в мили
+
+        :param kilometers (float): Километры
+        :return:
+                float: Мили
+        """
+        return kilometers * 0.62137
