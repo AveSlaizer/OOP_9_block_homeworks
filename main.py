dacefec7db93ea8497b0b52620f31dbfc0c42b72
Sushkov AS <aveslaizer@gmail.com>
2023-03-30 20:59:57 +0300

    Протестировал класс Passport

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Passport:
                f"Номер: {self.__number}"
 
     def info(self):
-        print(f"Класс: {self.__class__.__name__}\n"
+        print(f"\nКласс: {self.__class__.__name__}\n"
               f"ФИО: {list(self.__name.values())}\n"
               f"Пол: {self.__gender}\n"
               f"Дата рождения: {list(self.__date_of_birthday.values())}\n"
@@ -46,7 +46,7 @@ class ForeignPassport(Passport):
                  place_of_birth: str = None, citizenship: str = None, date_of_issue: Dict[str, str] = None,
                  expiration_date: Dict[str, str] = None, issued_at: str = None, series: str = None, number: str = None,
                  visas: List[object] = None):
-        super.__init__(name, gender, date_of_birthday, place_of_birth)
+        super().__init__(name, gender, date_of_birthday, place_of_birth)
         self.__citizenship = citizenship
         self.__date_of_issue = date_of_issue
         self.__expiration_date = expiration_date
@@ -57,7 +57,21 @@ class ForeignPassport(Passport):
 
 
 def execute_application():
-    pass
+    name = {"имя": "Артем", "Фамилия": "Сушков", "Отчество": "Сергеевич"}
+    gender = "м"
+    date_of_birthday = {"день": "11", "месяц": "апрель", "год": "1988"}
+    place_of_birth = "г. Ярославль"
+    address = {"улица": "Пушкина", "дом": "3"}
+    date_of_issue = {"день": "30", "месяц": "апрель", "год": "2000"}
+    issued_at = "Ярославское РОВД"
+    series = "1234"
+    number = "123456789"
+
+    passport = Passport(name, gender, date_of_birthday, place_of_birth,
+                        address, date_of_issue, issued_at, series, number)
+
+    print(passport)
+    passport.info()
 
 
 if __name__ == "__main__":
