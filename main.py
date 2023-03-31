7c2cc20378f5fe58b6ccfbc2208054bd2769bbf6
Sushkov AS <aveslaizer@gmail.com>
2023-03-31 20:28:57 +0300

    Написал класс Device

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+"""
+Задание 1.
+Создайте класс Device, который содержит информацию об устройстве.
+С помощью механизма наследования, реализуйте класс CoffeeMachine
+(содержит информацию о кофемашине), класс Blender (содержит информацию
+о блендере).
+"""
+
+
+class Device:
+
+    def __init__(self, voltage: str, brand: str, kind: str):
+        self.__voltage = voltage
+        self.__brand = brand
+        self.__kind = kind
+
 
 
 def execute_application():
