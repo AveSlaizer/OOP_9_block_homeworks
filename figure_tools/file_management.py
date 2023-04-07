a937ab940dc7d2ec84e2649ab03b9f1507748f1f
Sushkov AS <aveslaizer@gmail.com>
2023-04-08 00:44:13 +0300

    Написал абстрактный класс FigureFileManagement

--- figure_tools/file_management.py
+++ figure_tools/file_management.py
@@ -1,2 +1,14 @@
-class FigureFileManagement:
-    pass
+from figures import Square, Circle, Ellipse
+from abc import ABC, abstractmethod
+from typing import Union
+
+
+class FigureFileManagement(ABC):
+
+    @abstractmethod
+    def write_in_file(self, figure: Union[Square, Circle, Ellipse], path: str):
+        pass
+
+    @abstractmethod
+    def read_from_file(self, path) -> Union[Square, Circle, Ellipse]:
+        pass
