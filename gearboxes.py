a10d06d46797ee26348c5acd9bf39fb1fbc65f5c
Sushkov AS <aveslaizer@gmail.com>
2023-04-16 12:49:22 +0300

    Дополнил класс Transmission

--- gearboxes.py
+++ gearboxes.py
@@ -1,3 +1,9 @@
+class InvalidSpeed(Exception):
+
+    def __init__(self, text: str):
+        self.text = text
+
+
 class Transmission:
 
     def __init__(self, model: str, shift_type: str, speed_qty: int, speed: int = 0):
@@ -7,10 +13,17 @@ class Transmission:
         self.__speed = speed
 
     def __str__(self):
-        return f"Модель: {self.__model}, тип: {self.__shift_type}"
+        return f"Модель: {self.__model}, тип: {self.__shift_type}, количество передач: {self.__speed_qty}"
 
     def gear_up(self):
-        pass
+        if self.__speed == self.__speed_qty:
+            raise InvalidSpeed("Невозможно повысить передачу")
+        self.__speed += 1
+
+    def gear_down(self):
+        if self.__speed == -1:
+            raise InvalidSpeed("Невозможно понизить передачу")
+        self.__speed -= 1
 
     @property
     def model(self):
@@ -20,15 +33,12 @@ class Transmission:
     def shift_type(self):
         return self.__shift_type
 
-
-
-"""
-    def get_status(self):
+    def status(self):
         if self.__speed == -1:
             return "Включена задняя передача"
         elif self.__speed == 0:
             return "КПП в нейтральном положении"
         else:
             return f"Включена {self.__speed}-я передача"
-"""
+
 
