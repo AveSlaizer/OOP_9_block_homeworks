caeb344c487b55d17f6013fc9a5faf364a0a92f3
Sushkov AS <aveslaizer@gmail.com>
2023-04-16 12:54:45 +0300

    Исправил класс Engine

--- engines.py
+++ engines.py
@@ -8,20 +8,10 @@ class Engine:
     def __str__(self):
         return f"Модель: {self.__model}, Мощность: {self.__power}"
 
-    @property
-    def model(self):
-        return self.__model
-
-    @property
-    def power(self):
-        return self.__power
-
-    @property
-    def error_list(self):
-        if not self.__error_list:
-            return
-        return self.__error_list
-
-
     def add_error(self, error_code: str):
         self.__error_list.append(error_code)
+
+    def get_failure_status(self):
+        if not self.__error_list:
+            return "Ошибки не обнаружены, двигатель работает в штатном режиме"
+        return "Обнаружены ошибки в работе двигателя, обратитесь в сервисный центр"
