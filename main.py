ff0bd1d72399a3d4b3a97b8472cead57219eb61c
Sushkov AS <aveslaizer@gmail.com>
2023-04-25 23:00:45 +0300

    Написал и протстировал методы сравнения объектов класса Circle

--- main.py
+++ main.py
@@ -24,8 +24,44 @@ class Circle:
     def lenght(self):
         return self.__length
 
+    def __is_circle(self, other: object):
+        if not isinstance(other, Circle):
+            raise TypeError(f"Невозможно выполнить сравнение между типом {self.__class__.__name__} "
+                            f"и {other.__class__.__name__}")
+
+    def __hash__(self):
+        return hash((self.__radius, self.__length))
+
+    def __eq__(self, other):
+        self.__is_circle(other)
+        return self.__radius == other.__radius
+
+    def __ne__(self, other):
+        self.__is_circle(other)
+        return self.__radius != other.__radius
+
+    def __lt__(self, other):
+        self.__is_circle(other)
+        return self.__length < other.__length
+
+    def __le__(self, other):
+        self.__is_circle(other)
+        return self.__length <= other.__length
+
+    def __gt__(self, other):
+        self.__is_circle(other)
+        return self.__length > other.__length
+
+    def __ge__(self, other):
+        self.__is_circle(other)
+        return self.__length >= other.__length
+
 def execute_application():
-    pass
+
+    circle1 = Circle(2)
+    circle2 = Circle(3)
+
+    print(circle1 >= circle2)
 
 
 if __name__ == "__main__":
