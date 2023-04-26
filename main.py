62932e6323781d7f034e25b97dde2f07791ade70
Sushkov AS <aveslaizer@gmail.com>
2023-04-26 20:20:55 +0300

    Написал класс Point и методы сравнения пар координат

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+"""
+Задание 2.
+Создайте класс Point (точка). Для данного класса реализуйте ряд
+перегруженных операторов:
+Проверка на равенство пар координат по осям X и Y (операция ==, !=);
+Проверка сравнения пар координат (операции >, <, <=, >=);
+"""
+
+
+class Point:
+
+    def __init__(self, x: int, y: int):
+        self.__x = x
+        self.__y = y
+
+    def __is_point(self, other):
+        if not isinstance(other, Point):
+            raise TypeError(f"Невозможно выполнить сравнение между типом {self.__class__.__name__} "
+                            f"и {other.__class__.__name__}")
+
+    def __hash__(self):
+        return hash((self.__x, self.__y))
+
+    def __eq__(self, other):
+        self.__is_point(other)
+        return self.__x == other.__x and self.__y == other.__y
+
+    def __ne__(self, other):
+        self.__is_point(other)
+        return self.__x != other.__x or self.__y != other.__y
+
+
 def execute_application():
     pass
 
