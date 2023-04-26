5923b77673d5d6d485b6123570f77feb94ad02ed
Sushkov AS <aveslaizer@gmail.com>
2023-04-26 21:15:59 +0300

    Дописал методы сравнения

--- main.py
+++ main.py
@@ -18,20 +18,51 @@ class Point:
             raise TypeError(f"Невозможно выполнить сравнение между типом {self.__class__.__name__} "
                             f"и {other.__class__.__name__}")
 
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __str__(self):
+        return f"({self.__x}, {self.__y})"
+
     def __hash__(self):
         return hash((self.__x, self.__y))
 
-    def __eq__(self, other):
+    def __eq__(self, other):  # ==
         self.__is_point(other)
         return self.__x == other.__x and self.__y == other.__y
 
-    def __ne__(self, other):
+    def __ne__(self, other):  # !=
         self.__is_point(other)
         return self.__x != other.__x or self.__y != other.__y
 
+    def __lt__(self, other):  # <
+        self.__is_point(other)
+        return self.__x < other.__x and self.__y < other.__y
+
+    def __gt__(self, other):  # >
+        self.__is_point(other)
+        return self.__x > other.__x and self.__y > other.__y
+
+    def __le__(self, other):
+        self.__is_point(other)
+        return self.__x <= other.__x and self.__y <= other.__y
+
+    def __ge__(self, other):
+        self.__is_point(other)
+        return self.__x >= other.__x and self.__y >= other.__y
 
 def execute_application():
-    pass
+
+
+    point1 = Point(3, 3)
+    point2 = Point(3, 4)
+    print(point1 <= point2)
+    print(point1 >= point2)
 
 
 if __name__ == "__main__":
