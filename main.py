78ec801ddd0966218a895de1bcde8971a56ba544
Sushkov AS <aveslaizer@gmail.com>
2023-05-02 21:37:12 +0300

    Написал абстрактные классы

--- main.py
+++ main.py
@@ -1,3 +1,50 @@
+from abc import ABC, abstractmethod
+
+"""
+Задание 1.
+Рассмотрим принцип разделения интерфейсов.
+Допустим у нас имеется абстрактный класс Payments и в нем есть три метода:
+оплата WebMoney, оплата банковской карточкой и оплата по номеру телефона.
+
+class Payments(ABC):
+    @abstarctmethod
+    def payWebMoney(self):
+        pass
+
+    @abstarctmethod
+    def payCreditCard(self):
+        pass
+
+    @abstarctmethod
+    def payPhoneNumber(self):
+        pass
+
+Выполните разделение интерфейса таким образом, чтобы можно было реализовать
+два класса-сервиса, которые будут у себя реализовывать различные виды проведения
+оплат (класс InternetPaymentService и TerminalPaymentService). При этом
+TerminalPaymentService не должен поддерживать проведение оплат по номеру телефона.
+"""
+
+
+class WedMoneyPayment(ABC):
+
+    @abstractmethod
+    def pay_web_money(self):
+        pass
+
+
+class CreditCardPayment(ABC):
+
+    @abstractmethod
+    def pay_credit_card(self):
+        pass
+
+
+class PhoneNumberPayment(ABC):
+
+    @abstractmethod
+    def pay_phone_number(self):
+        pass
 
 
 def execute_application():
