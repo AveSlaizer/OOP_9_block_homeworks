7a0a0088223c1046768e047c1c2bbac8abb98b99
Sushkov AS <aveslaizer@gmail.com>
2023-05-10 22:13:47 +0300

    Написал класс AirPlanePickleAdapter

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import pickle
+
+
 """
 Задание 1.
 Создайте класс «Самолет». Наполните его необходимыми характеристиками и методами. Реализуйте упаковку и
-распаковку объектов класса «Самолет» с использованием
-модуля pickle.
+распаковку объектов класса «Самолет» с использованием модуля pickle.
 """
 
 
@@ -27,6 +29,30 @@ class AirPlane:
     @property
     def engine_qty(self):
         return self.__engine_qty
+
+class AirPlanePickleAdapter:
+
+    @staticmethod
+    def to_pickle(plane: AirPlane):
+        if not isinstance(plane, AirPlane):
+            raise TypeError(f"Недопустимый тип данных {plane.__class__.__name__}, ожидался 'AirPlane'")
+        return pickle.dumps({
+            "className": plane.__class__.__name__,
+            "name": plane.name,
+            "engine_type": plane.engine_type,
+            "engine_qty": plane.engine_qty
+        })
+
+    @staticmethod
+    def from_pickle(data):
+        obj = pickle.loads(data)
+        try:
+            if obj["className"] == "AirPlane":
+                return AirPlane(obj["name"], obj["engine_type"], obj["engine_qty"])
+        except AttributeError as e:
+            print(e)
+
+
 
 
 def execute_application():
