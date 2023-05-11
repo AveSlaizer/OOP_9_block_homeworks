d496c4f7fc0be49106887dc9206c77d0f12370d1
Sushkov AS <aveslaizer@gmail.com>
2023-05-11 21:32:25 +0300

    Откатил к стартовому шаблону

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-from fraction import MathematicalFraction, FractionPickleAdapter, FractionJSONAdapter
-
-"""
-Задание 3
-К уже реализованному классу «Дробь» добавьте возможность 
-упаковки и распаковки данных с использованием json и pickle
-"""
-
-
 def execute_application():
-     fraction = MathematicalFraction(1, 2)
-     print(fraction)
-     data = FractionJSONAdapter.to_json(fraction)
-     print(data)
-     new_fraction = FractionJSONAdapter.from_json(data)
-     print(new_fraction)
+    pass
 
 
 if __name__ == "__main__":
