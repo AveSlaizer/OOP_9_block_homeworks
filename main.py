a0ef4cfac854b57bc69389f7a5d51098c8f22637
Sushkov AS <aveslaizer@gmail.com>
2023-05-14 16:35:07 +0300

    Написал класс Car

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import pickle, json
+
+"""
+Задание 1
+К уже реализованному классу «Автомобиль» добавьте
+возможность упаковки и распаковки данных с использованием json и pickle.
+"""
+
+
+class Car:
+
+    def __init__(self, brand: str, model: str, year: str):
+        self.__brand = brand
+        self.__model = model
+        self.__year = year
+
+    def __str__(self):
+        return f"{self.__brand} {self.__model}, {self.__year} года выпуска"
 
 
 def execute_application():
