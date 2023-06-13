23a7d4e62894fec51fdcf0a280c414acae72c8c9
Sushkov AS <aveslaizer@gmail.com>
2023-06-13 17:51:31 +0300

    Написал методы перевода в системы счисления в классе NSCalculator

--- main.py
+++ main.py
@@ -26,9 +26,12 @@ class Digit:
         if not (2 <= number_system <= 16):
             raise ValueError(f"Недопустимое значение системы счисления. Ожидалось число от 2 до 16.")
         for symbol in value:
-            if symbol not in Digit.__symbols[:number_system]:
+            if symbol.lower() not in Digit.__symbols[:number_system]:
                 raise ValueError(f"Недопустимое числовое значение в {number_system}-ой системе счисления.")
         return value, number_system
+
+    def __index__(self):
+        return int(self.__value, self.__number_system)
 
     @property
     def value(self):
@@ -42,96 +45,27 @@ class Digit:
 class NSCalculator:
 
     @staticmethod
-    def __is_dec(number: Digit):
-        if not number.number_system == "dec":
-            raise ValueError(f"Недопустимое значение системы счисления '{number.number_system}' ожидалось 'dec'")
+    def to_hex(digit: Digit) -> Digit:
+        return Digit(hex(digit)[2:], 16)
 
     @staticmethod
-    def __is_bin(number: Digit):
-        if not number.number_system == "bin":
-            raise ValueError(f"Недопустимое значение системы счисления '{number.number_system}' ожидалось 'bin'")
+    def to_oct(digit: Digit) -> Digit:
+        return Digit(oct(digit)[2:], 8)
 
     @staticmethod
-    def __is_oct(number: Digit):
-        if not number.number_system == "oct":
-            raise ValueError(f"Недопустимое значение системы счисления '{number.number_system}' ожидалось 'oct'")
+    def to_bin(digit: Digit) -> Digit:
+        return Digit(bin(digit)[2:], 2)
 
     @staticmethod
-    def __is_hex(number: Digit):
-        if not number.number_system == "hex":
-            raise ValueError(f"Недопустимое значение системы счисления '{number.number_system}' ожидалось 'hex'")
-
-    @staticmethod
-    def dec_to_oct(number: Digit):
-        NSCalculator.__is_dec(number)
-        value = oct(int(number.value))[2:]
-        number_system = "oct"
-        return Digit(value, number_system)
-
-    @staticmethod
-    def dec_to_bin(number: Digit):
-        NSCalculator.__is_dec(number)
-        value = bin(int(number.value))[2:]
-        number_system = "bin"
-        return Digit(value, number_system)
-
-    @staticmethod
-    def dec_to_hex(number: Digit):
-        NSCalculator.__is_dec(number)
-        value = hex(int(number.value))[2:]
-        number_system = "hex"
-        return Digit(value, number_system)
-
-    @staticmethod
-    def bin_to_dec(number: Digit):
-        NSCalculator.__is_bin(number)
-        value = str(int(number.value, 2))
-        number_system = "dec"
-        return Digit(value, number_system)
-
-    @staticmethod
-    def oct_to_dec(number: Digit):
-        NSCalculator.__is_oct(number)
-        value = str(int(number.value, 8))
-        number_system = "dec"
-        return Digit(value, number_system)
-
-    @staticmethod
-    def hex_to_dec(number: Digit):
-        NSCalculator.__is_hex(number)
-        value = str(int(number.value, 16))
-        number_system = "dec"
-        return Digit(value, number_system)
-
-    @staticmethod
-    def bin_to_oct(number: Digit):
-        return NSCalculator.dec_to_oct(NSCalculator.bin_to_dec(number))
-
-    @staticmethod
-    def bin_to_hex(number: Digit):
-        return NSCalculator.dec_to_hex(NSCalculator.bin_to_dec(number))
-
-    @staticmethod
-    def oct_to_bin(number: Digit):
-        return NSCalculator.dec_to_bin(NSCalculator.oct_to_dec(number))
-
-    @staticmethod
-    def oct_to_hex(number: Digit):
-        return NSCalculator.dec_to_hex(NSCalculator.oct_to_dec(number))
-
-    @staticmethod
-    def hex_to_bin(number: Digit):
-        return NSCalculator.dec_to_bin(NSCalculator.hex_to_dec(number))
-
-    @staticmethod
-    def hex_to_oct(number: Digit):
-        return NSCalculator.dec_to_oct(NSCalculator.hex_to_dec(number))
-
-
+    def to_dec(digit: Digit) -> Digit:
+        return Digit(str(int(digit)), 10)
 
 
 def execute_application():
-    numb = Digit("16", 16)
+    numb = Digit("11", 2)
+    print(numb.__dict__)
+    numb = NSCalculator.to_dec(numb)
+    print(numb.__dict__)
 
 
 if __name__ == "__main__":
